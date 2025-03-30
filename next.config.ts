3ccb04e1acf8ead02127725750d4fdf7be4ee9da
Yuri Garcia Ribeiro <81641949+YuriGarciaRibeiro@users.noreply.github.com>
2025-03-30 18:51:44 -0300

    Update Next.js configuration for export mode and add base path

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  output: "export",
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ['avatars.githubusercontent.com'],
+    unoptimized: true,
   },
+  basePath: "/yuri-garcia-portifolio",
 };
 export default nextConfig;
